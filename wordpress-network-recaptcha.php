58ff1930a00b777b289d1b008a1ab15368192406
Jimmy Sjunfors <jimmy@find.eu>
2020-12-31 15:35:24 +0100

    fix

--- wordpress-network-recaptcha.php
+++ wordpress-network-recaptcha.php
@@ -16,14 +16,14 @@ defined('ABSPATH') or die();
 */
 $wpnre_sitekey = '';
 $wpnre_privatekey = '';
-$wpnre_secure_ips = array("");
+$wpnre_secure_ips = array('');
 
 
 /*
     LANGUAGE SETTINGS FOR CUSTOM DIE MESSAGE
 */
-$wpnre_die_message1 = "reCAPTCHA validation failed.";
-$wpnre_die_message2 = "Return to login and try again.";
+$wpnre_die_message1 = 'reCAPTCHA validation failed.';
+$wpnre_die_message2 = 'Return to login and try again.';
 
 
 /*
@@ -46,10 +46,12 @@ function wpnre_add_to_head() {
 }
 
 function wpnre_auth_signon() {
-    global $wpnre_privatekey;
+    global $wpnre_privatekey,$wpnre_die_message1,$wpnre_die_message2;
+
     $wpnreerror = false;
-    if (isset($_POST["log"]) && isset($_POST["pwd"]) && !empty($_POST["log"]) && !empty($_POST["pwd"])) {
-        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+
+    if (isset($_POST['log']) && isset($_POST['pwd']) && !empty($_POST['log']) && !empty($_POST['pwd'])) {
+        if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
             $response = wp_remote_post('https://www.google.com/recaptcha/api/siteverify', array(
                 'method' => 'POST',
                 'body' => array(
@@ -58,25 +60,25 @@ function wpnre_auth_signon() {
                 )
             ));
 
-            $response = json_decode($response["body"], true);
+            $response = json_decode($response['body'], true);
 
-            if (true == $response["success"]) {
-                if ($response["hostname"] == $_SERVER["HTTP_HOST"]) {
+            if (true == $response['success']) {
+                if ($response['hostname'] == $_SERVER['HTTP_HOST']) {
                     $wpnreerror = false;
                 } else {
-                    $wpnreerror = "Invalid reCAPTCHA hostname check";
+                    $wpnreerror = 'Invalid reCAPTCHA hostname check';
                 }
             } else {
-                $wpnreerror = "Invalid reCAPTCHA (1)";
+                $wpnreerror = 'Invalid reCAPTCHA (1)';
             }
         } else {
-            $wpnreerror = "Invalid reCAPTCHA (2)";
+            $wpnreerror = 'Invalid reCAPTCHA (2)';
         }
 
         if ($wpnreerror) {
-            $diemessage = "<center><br><br>".$wpnre_die_message1."<br><br>
-            Error: ".$wpnreerror."<br><br>
-            <a href='/wp-admin'>".$wpnre_die_message2."</a>";
+            $diemessage = '<center><br><br>'.$wpnre_die_message1.'<br><br>
+            Error: '.$wpnreerror.'<br><br>
+            <a href="/wp-admin">'.$wpnre_die_message2.'</a>';
 
             DIE($diemessage);
         } else {
